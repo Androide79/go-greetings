7e0f2be11c9d353cb0d8371cb0f41a6d2a8a1145
Pierluigi <androide79@gmail.com>
2020-06-11 14:24:10 +0200

    clean

--- greetings.go
+++ greetings.go
@@ -4,14 +4,6 @@ import (
 	"fmt"
 )
 
-// func main() {
-// 	var emptyName string = ""
-// 	var name string = "Pigi"
-
-// 	fmt.Println(greetings(emptyName)) // this will return "Hello Dude!"
-// 	fmt.Println(greetings(name)) // this will return "Hello Pigi!"
-// }
-
 func Greet(name string) string {
 	var result string
 
