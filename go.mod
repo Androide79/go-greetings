2d4f058114770cc222d494c4d3ea61d699359c5c
Pierluigi <androide79@gmail.com>
2020-06-11 10:31:14 +0200

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Androide79/go-greetings
+
+go 1.14
---
+++ greetings.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var emptyName string = ""
+	var name string = "Pigi"
+
+	fmt.Println(greetings(emptyName)) // this will return "Hello Dude!"
+	fmt.Println(greetings(name)) // this will return "Hello Pigi!"
+}
+
+func greetings(name string) string {
+	var result string
+
+	if len(name) == 0 {
+		result = "Hello Dude!"
+	} else {
+		result = fmt.Sprintf("Hello %v!", name)
+	}
+
+	return result
+}
